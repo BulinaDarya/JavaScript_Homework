b7ab9922d1d9b49585b4d1de0282bc8140c66473
Darya <bulinadarya@mail.ru>
2023-03-14 12:16:05 +0300

    Add files via upload
    Homework10

---
+++ Work10/data.js
@@ -0,0 +1,38 @@
+const dataInfo =  `[
+    {
+      "API": "AdoptAPet",
+      "Description": "Resource to help get pets adopted",
+      "Auth": "apiKey",
+      "HTTPS": true,
+      "Cors": "yes",
+      "Link": "https://www.adoptapet.com/public/apis/pet_list.html",
+      "Category": "Animals"
+    },
+    {
+      "API": "Cat Facts",
+      "Description": "Daily cat facts",
+      "Auth": "_",
+      "HTTPS": true,
+      "Cors": "no",
+      "Link": "https://alexwohlbruck.github.io/cat-facts/",
+      "Category": "Animals"
+    },
+    {
+      "API": "eBird",
+      "Description": "Retrieve recent or notable birding observations within a region",
+      "Auth": "apiKey",
+      "HTTPS": true,
+      "Cors": "no",
+      "Link": "https://documenter.getpostman.com/view/664302/S1ENwy59",
+      "Category": "Animals"
+    },
+    {
+      "API": "PlaceKitten",
+      "Description":"Placeholder Kitten pictures",
+      "Auth": "_",
+      "HTTPS": true,
+      "Cors": "yes",
+      "Link": "https://placekitten.com/",
+      "Category": "Animals"
+    }
+]`
